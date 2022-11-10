0f291f0fb915142daa69a6e746c46d20ab0384c8
Dominic Hock <d.hock@it-hock.de>
2022-11-10 10:41:53 +0100

    chore(plugin): add icon

---
+++ src/main/kotlin/de/ithock/fishlang/FishIcons.java
@@ -0,0 +1,10 @@
+package de.ithock.fishlang;
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class FishIcons {
+
+    public static final Icon FILE = IconLoader.getIcon("pluginIcon.svg", FishIcons.class);
+
+}
